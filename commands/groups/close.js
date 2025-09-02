1813335c86b0f53aaaeb291a54f2e9d430319746
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-02 01:01:22 -0300

    Create close.js

---
+++ commands/groups/close.js
@@ -0,0 +1,18 @@
+// codigo creado por: Chinita 🇨🇳
+module.exports = {
+  command: ["close", "cerrar"],
+  description: "Cierra el grupo (solo admins pueden hablar)",
+  category: "groups",
+  isGroup: true,
+  isAdmin: true,
+  botAdmin: true,
+  run: async (client, m) => {
+    try {
+      await client.groupSettingUpdate(m.chat, "announcement");
+      m.reply("🔒 El grupo ahora está *cerrado* .");
+    } catch (e) {
+      console.error(e);
+      m.reply("❌ No se pudo cerrar el grupo");
+    }
+  },
+};
